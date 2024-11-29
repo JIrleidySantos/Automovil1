e53e4a83b4f53a9267bd387ca430320596d00e01
JIrleidySantos <jirleidysantosm@gmail.com>
2024-11-29 17:17:39 -0500

    Creacion

---
+++ Carros/src/main/java/ec/edu/espoch/carros/Carros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.carros;
+
+/**
+ *
+ * @author mundo
+ */
+public class Carros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello W  orld!");
+    }
+}
